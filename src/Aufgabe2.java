9fd475e3ca1f1c364cfc1cedf31605bacdcaa8d4
nbochejny <nbochejny@gmail.com>
2018-12-05 15:06:34 +0100

    AufgabeNr3

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -3,6 +3,15 @@ import javax.swing.*;
 public class Aufgabe2 {
     public static void main(String[]args){
         partialString(JOptionPane.showInputDialog(null,"Gib was ein"));
+        //Zuerst Klasse dann . und danach die methode anwählen (Klasse.methode)(int/double variablen einsetzen)
+        System.out.println(Taschenrechner.addition(265,401));
+        System.out.println(Taschenrechner.addition(2.65,4.01));
+        System.out.println(Taschenrechner.subtraction(1000,445));
+        System.out.println(Taschenrechner.subtraction(10,4.45));
+        System.out.println(Taschenrechner.multiplication(111,6));
+        System.out.println(Taschenrechner.multiplication(1.11,6));
+        System.out.println(Taschenrechner.division(7215,13));
+        System.out.println(Taschenrechner.division(7.215,1.3));
     }
     private static void partialString(String a){
         if (a.length()<3){
---
+++ src/Taschenrechner.java
@@ -0,0 +1,26 @@
+public class Taschenrechner {
+    public static int addition(int a,int b){
+        return a+b;
+    }
+    public static double addition(double a, double b){
+        return a+b;
+    }
+    public static int subtraction(int a,int b){
+        return a-b;
+    }
+    public static double subtraction(double a,double b){
+        return a-b;
+    }
+    public static int multiplication(int a,int b){
+        return a*b;
+    }
+    public static double multiplication(double a, double b){
+        return a*b;
+    }
+    public static int division(int a, int b){
+        return a/b;
+    }
+    public static double division(double a, double b){
+        return a/b;
+    }
+}
